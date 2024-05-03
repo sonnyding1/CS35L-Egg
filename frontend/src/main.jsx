0e34ba0a529a0be3967502c26de0d2ade0f88e36
Sonny Ding <sonnyding20@gmail.com>
2024-05-02 20:17:50 -0700

    feat: Add react-router-dom dependency for routing in the frontend

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Community from "./pages/Community.jsx";
+import Edit from "./pages/Edit.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/community",
+    element: <Community />,
+  },
+  {
+    path: "/edit",
+    element: <Edit />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
---
+++ frontend/src/pages/Community.jsx
@@ -0,0 +1,9 @@
+function Community() {
+  return (
+    <div>
+      <h1>Community</h1>
+    </div>
+  );
+}
+
+export default Community;
