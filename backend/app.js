1742163f8aa6f3ce91699d4ed2b1acd305c4dd51
Sonny Ding <sonnyding20@gmail.com>
2024-05-12 16:21:41 -0700

    chore: setup mongodb

--- backend/app.js
+++ backend/app.js
@@ -3,10 +3,21 @@ const session = require("express-session");
 const cors = require("cors");
 const passport = require("passport");
 const authRoutes = require("./routes/authRoutes");
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// connect to mongodb
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 // middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
