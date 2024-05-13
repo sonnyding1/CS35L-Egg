661d60f4631073387b0fb14046001dd390308ad6
Gonfreecs17 <waihunsann@gmail.com>
2024-05-13 01:14:27 -0700

    Added navigation.jsx containing a skeleton navigation bar. Amended Community.jsx and Home.jsx to accompany the navigation bar.

---
+++ frontend/src/components/ui/navigation.jsx
@@ -0,0 +1,56 @@
+// Navigation.js
+
+import React from 'react';
+import { NavLink } from 'react-router-dom'; // Using React Router
+
+
+export default function Navigation() {
+  return (
+    //Commenting out the Edit page. 
+    <div className="container mx-auto bg-slate-700 drop-shadow-2xl p-8
+    grid grid-cols-3">
+      <div>
+    <nav>
+      
+      <button className="bg-neutral-100 p-3 rounded-2xl focus:ring-2 
+      transition-all ease-in-out delay-150 hover:bg-neutral-200 hover:overline hover:decoration-slate-900
+      text-slate-700 hover:text-slate-900
+      mr-4
+      ">
+        
+        <NavLink to="/" activeclassname="active">
+          <b>Home </b> 
+        </NavLink>
+
+      </button>
+
+      
+      {/* <li>    
+        <NavLink to="/Edit" activeclassname="active">
+          Edit
+        </NavLink>
+      </li> */}
+
+      <button className="bg-neutral-100 p-3 rounded-2xl focus:ring-2 
+      transition-all ease-in-out delay-150 hover:bg-neutral-200 hover:overline hover:decoration-slate-900
+      text-slate-700 hover:text-slate-900
+      ">
+        <NavLink to="/Community" activeclassname="active">
+          <b>Community</b>
+        </NavLink>
+      </button>
+    </nav>
+
+    </div>
+
+    <div></div> {/* wasting the second column*/}
+
+    <div className="flex justify-end"> 
+    <p className="text-slate-100 font-serif text-4xl">
+       Egg 
+    </p>
+    </div>
+
+    </div>
+  );
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
+import  Navigation from "@/components/ui/navigation";
+
 
 function Home() {
   return (
     <div>
+      <Navigation />
       <p className="text-red-500 text-3xl">Home</p>
       <Button>Click me</Button>
     </div>
