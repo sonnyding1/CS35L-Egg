4e4c68066b7d06ad15c99f62d0bdf9029cffe03c
Sonny Ding <sonnyding20@gmail.com>
2024-05-14 15:22:03 -0700

    feat: introduce Swagger, give docs to the 2 test endpoints

--- backend/app.js
+++ backend/app.js
@@ -6,6 +6,8 @@ const authRoutes = require("./routes/authRoutes");
 const mongoose = require("mongoose");
 const User = require("./models/User");
 require("dotenv").config();
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -19,6 +21,29 @@ mongoose
   .catch((err) => {
     console.error("Error connecting to MongoDB:", err);
   });
+
+// Swagger
+const swaggerDefinition = {
+  openapi: "3.0.0",
+  info: {
+    title: "Egg API",
+    version: "1.0.0",
+    description: "This is the backend API for the Egg application.",
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "Development server",
+    },
+  ],
+};
+const options = {
+  swaggerDefinition,
+  // include relative paths to files containing OpenAPI definitions
+  apis: ["./app.js", "./models/*.js"],
+};
+const swaggerSpec = swaggerJsdoc(options);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 // middlewares
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
@@ -42,10 +67,47 @@ app.get("/", (req, res) => {
 
 // the below 2 routes are for demonstration purposes
 // that we have connected to the db
+/**
+ * @swagger
+ * /users:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     description: Retrieve a list of users from the database.
+ *     responses:
+ *       200:
+ *         description: A list of users.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/User'
+ */
 app.get("/users", async (req, res) => {
   const users = await User.find();
   res.json(users);
 });
+
+/**
+ * @swagger
+ * /users:
+ *   post:
+ *     summary: Create a new user
+ *     description: Create a new user and save it to the database.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *     responses:
+ *       200:
+ *         description: The created user.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/User'
+ */
 app.post("/users", async (req, res) => {
   const user = new User(req.body);
   await user.save();
