211ed45b4e76b6dde093d64df25f52e27b7e281e
Sonny Ding <sonnyding20@gmail.com>
2024-05-13 18:38:48 -0700

    feat: introduct shadcn to navbar

--- frontend/src/components/ui/navigation.jsx
+++ frontend/src/components/ui/navigation.jsx
@@ -1,56 +1,58 @@
 // Navigation.js
 
-import React from 'react';
-import { NavLink } from 'react-router-dom'; // Using React Router
-
+import React from "react";
+import { NavLink } from "react-router-dom"; // Using React Router
+import { Button } from "./button";
 
 export default function Navigation() {
   return (
-    //Commenting out the Edit page. 
-    <div className="container mx-auto bg-slate-700 drop-shadow-2xl p-8
-    grid grid-cols-3">
+    //Commenting out the Edit page.
+    <div
+      className="container mx-auto bg-slate-700 drop-shadow-2xl p-8
+    grid grid-cols-3"
+    >
       <div>
-    <nav>
-      
-      <button className="bg-neutral-100 p-3 rounded-2xl focus:ring-2 
+        <nav>
+          {/* <button
+            className="bg-neutral-100 p-3 rounded-2xl focus:ring-2 
       transition-all ease-in-out delay-150 hover:bg-neutral-200 hover:overline hover:decoration-slate-900
       text-slate-700 hover:text-slate-900
       mr-4
-      ">
-        
-        <NavLink to="/" activeclassname="active">
-          <b>Home </b> 
-        </NavLink>
+      "
+          >
+            <NavLink to="/" activeclassname="active">
+              <b>Home </b>
+            </NavLink>
+          </button> */}
 
-      </button>
+          <Button variant="sun">
+            <NavLink to="/" activeclassname="active">
+              <b>Home </b>
+            </NavLink>
+          </Button>
 
-      
-      {/* <li>    
+          {/* <li>    
         <NavLink to="/Edit" activeclassname="active">
           Edit
         </NavLink>
       </li> */}
 
-      <button className="bg-neutral-100 p-3 rounded-2xl focus:ring-2 
+          <button
+            className="bg-neutral-100 p-3 rounded-2xl focus:ring-2 
       transition-all ease-in-out delay-150 hover:bg-neutral-200 hover:overline hover:decoration-slate-900
       text-slate-700 hover:text-slate-900
-      ">
-        <NavLink to="/Community" activeclassname="active">
-          <b>Community</b>
-        </NavLink>
-      </button>
-    </nav>
-
-    </div>
-
-    <div></div> {/* wasting the second column*/}
-
-    <div className="flex justify-end"> 
-    <p className="text-slate-100 font-serif text-4xl">
-       Egg 
-    </p>
-    </div>
-
+      "
+          >
+            <NavLink to="/Community" activeclassname="active">
+              <b>Community</b>
+            </NavLink>
+          </button>
+        </nav>
+      </div>
+      <div></div> {/* wasting the second column*/}
+      <div className="flex justify-end">
+        <p className="text-slate-100 font-serif text-4xl">Egg</p>
+      </div>
     </div>
   );
 }
