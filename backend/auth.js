ef5bbc16c3d1888d27de63834d5b6f7532167a18
Emily Pham <phamemilym@gmail.com>
2024-06-03 05:37:26 -0700

    fix bug in handling non-existent Google user

--- backend/auth.js
+++ backend/auth.js
@@ -59,7 +59,7 @@ const googleAuthCallback = async (req, res) => {
   // in the future send a response to the frontend
   // res.redirect("http://localhost:3000");
   try {
-    newUser = await User.findOne({username: req.session.passport.user}).exec();
+    const newUser = await User.findOne({username: req.session.passport.user}).exec();
     req.session.userId = newUser._id;
     return res.json(req.session);
   } catch (error) {
